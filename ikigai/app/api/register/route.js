c0141ab98a930d58ca9c4569718d059db9b454f6
Quynh <qpham2594@gmail.com>
2024-02-13 14:07:08 -0500

    login/create API success

--- ikigai/app/api/register/route.js
+++ ikigai/app/api/register/route.js
@@ -1,28 +1,38 @@
+// pages/api/register/route.js
 import { NextResponse } from "next/server";
 import connectMongoDB from "@/lib/mongodb";
-import user from "@/database/models/user";
+import User from "@/database/models/user";
+
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
 
 export async function POST(req) {
   try {
     const { username, password } = await req.json();
-    await connectMongoDB()
-    const existingUser = await user.findOne({username})
+    await connectMongoDB();
+
+    const existingUser = await User.findOne({ username });
     if (existingUser) {
-      return NextResponse.json({message:"Username already exists."}, {status:400})
+      return NextResponse.json({ message: "Username already exists." }, { status: 400 });
     }
 
-    const newUser = new User ({
+    const newUser = new User({
       username,
       password
-    })
-    await newUser.save() 
+    });
+    await newUser.save();
 
     console.log("Username:", username);
     console.log("Password:", password);
 
-   // return NextResponse.json({ message: "Account is created" }, { status: 201 });
+    return NextResponse.json({ message: "Account is created" }, { status: 201 });
   } catch (error) {
     console.error("Unable to register:", error);
     return NextResponse.json({ message: "Unable to register" }, { status: 500 });
   }
 }
+
+
