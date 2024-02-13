704f35dcbce213435289cf25e5b58e99d03ad161
Quynh <qpham2594@gmail.com>
2024-02-13 14:57:16 -0500

    app/api/recipes update with success on POSTMAN

--- ikigai/app/api/recipes/route.js
+++ ikigai/app/api/recipes/route.js
@@ -1,21 +1,51 @@
 import axios from 'axios';
 
-export default async function handler(req, res) {
+export default async function recipesSearch(query) {
   try {
     const apiKey = process.env.API_KEY;
 
-    const ingredients = req.query.ingredients; // Extract ingredients from the request query
-    const maxTime = req.query.maxReadyTime; // Extract maxTime from the request query
-
-    const APILink = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${ingredients}&maxReadyTime=${maxTime}`;
+    const APILink = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}`;
 
     const response = await axios.get(APILink);
     const data = response.data;
     console.log(data)
 
-    res.status(200).json(data);
+    return data
   } catch (error) {
     console.error('Error fetching recipes:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 }
+
+export default async function recipesbyTime(maxTime) {
+    try {
+      const apiKey = process.env.API_KEY;
+  
+      const APILink = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&maxReadyTime=${maxTime}`;
+  
+      const response = await axios.get(APILink);
+      const data = response.data;
+      return data
+
+    } catch (error) {
+      console.error('Error fetching recipes:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+
+export default async function recipesbyID(recipeId) {
+    try {
+      const apiKey = process.env.API_KEY;
+  
+      const APILink = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`;
+  
+      const response = await axios.get(APILink);
+      const data = response.data;
+      return data
+
+    } catch (error) {
+      console.error('Error fetching recipes:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
