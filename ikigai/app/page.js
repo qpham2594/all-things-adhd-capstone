c52b33a71968871d13f4d5638ecec6792062b069
Quynh <qpham2594@gmail.com>
2024-04-16 16:10:02 -0400

    vercel client error

--- ikigai/app/page.js
+++ ikigai/app/page.js
@@ -1,17 +1,11 @@
 import Header from "@/components/header";
 import styles from '@/styles/page.module.css';
-import Head from 'next/head';
 
 
 export default function Home() {
   return (
     <main className={styles.body}>
       <Header />
-      <Head>
-        <title>Ikigai Homepage</title>
-        <meta name="description" content="Welcome to Ikigai, an app that can help you with your to-do list in the simplest way." />
-        <html lang="en" />
-      </Head>
       <div className={styles.pageWrapper}>
         <div className={styles.heroImage}>
           <div className={styles.overlay}>
--- ikigai/pages/login.jsx
+++ ikigai/pages/login.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { signIn } from 'next-auth/react';
 import styles from '@/styles/page.module.css';
-import Head from 'next/head';
+//import Head from 'next/head';
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
@@ -58,7 +58,6 @@ const LoginForm = () => {
       <Head>
         <title>Log in with Ikigai</title>
         <meta name="description" content="Make sure you sign in with Ikigai so you can work on your to-do list. If not, feel free to use the search recipes feature!" />
-        <html lang="en" />
       </Head>
       <form onSubmit={handleSubmit} className={styles.formBox}>
         <div className={styles.textWrapper}>
